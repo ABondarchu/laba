ad710899a402df94b185a4e602aaf130181329f7
ABondarchu <76469391+ABondarchu@users.noreply.github.com>
2020-12-22 16:48:48 +0300

    Update 5.2.c

--- фунт-5/5.2.c
+++ фунт-5/5.2.c
@@ -5,12 +5,10 @@ int main()
 {
     int row, col, i, j, e;
     unsigned short hasPositiveElement;
-    //ask user to specify the size of the matrix and write answer
     printf("Задайте количество строк матрицы: ");
     scanf("%d", &row);
     printf("Задайте количество столбцов матрицы: ");
     scanf("%d", &col);
-    //create cycle and ask user to specify each element of the matrix, write answer 
     int a[row][col];
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
@@ -18,7 +16,6 @@ int main()
             scanf("%d", &a[i][j]);
         }
     }
-    //check matrix if row with negative elements exists
     for (i = 0; i < row; i++) {
         hasPositiveElement = 0;
         for (j = 0; j < col; j++) {
@@ -27,17 +24,14 @@ int main()
                 break;
             }
         }
-        //remember the first element of the found string
         if (!hasPositiveElement) {
             e = a[i][0];
             break;
         }
     }
-    //create a cycle where we add the previously obtained value to each element of the matrix
     for (i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
             a[i][j] = a[i][j] + e;
-            //print updated matrix
             printf ("Преобразованный элемент [%d,%d] матрицы: %d \n", i, j, a[i][j]);
         }
     }
