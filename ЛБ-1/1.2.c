30ef64c39bccb64be27800b28f776fe684fbb3fd
ABondarchu <76469391+ABondarchu@users.noreply.github.com>
2020-12-22 16:44:13 +0300

    Update 1.2.c

--- ЛБ-1/1.2.c
+++ ЛБ-1/1.2.c
@@ -7,7 +7,7 @@ int main(void) {
 	
 	float x,y,z,a,b,c,s;
 	
-	printf("Vvedite zmachenija peremennix x, y, z ");
+	printf("Ввидите значение переменых  x, y, z ");
 	scanf("%f \n %f \n %f", &x, &y, &z);
  a = pow(9+ (pow((x-y),2)),1.0/3.0); 
  b = (pow(x,2.0) +pow(y,2.0)) +2;
@@ -15,7 +15,7 @@ int main(void) {
  s = (a/b)-c;  
 	
 
-    printf("Result = %f \n",s);
+    printf("Рузультат = %f \n",s);
 	
 	
 	
