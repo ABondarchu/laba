8f288a18deacaf9267ba3dacd0d264b30bb313e4
ABondarchu <76469391+ABondarchu@users.noreply.github.com>
2020-12-22 16:48:01 +0300

    Update 5.1.c

--- фунт-5/5.1.c
+++ фунт-5/5.1.c
@@ -15,7 +15,6 @@ void main() {
         minRow, minRowIndex, minColumnIndex
     ;
     unsigned short isNotTrueMinPoint, isNotTrueMaxPoint;
-    // check each row to find min and max
     for (i = 0; i < m; i++) {
         maxRow = a[i][0];
         maxRowIndex = i;
@@ -24,22 +23,18 @@ void main() {
         minRow = a[i][0];
         minRowIndex = i;
         minColumnIndex = 0;
-        // determinate max and min elements and indexes in row
         for (j = 1; j < n; j++) {
-            // find max element in row
             if (a[i][j] > maxRow) {
                 maxRow = a[i][j];
                 maxRowIndex = i;
                 maxColumnIndex = j;
             }
-            // find min element in row
             if (a[i][j] < minRow) {
                 minRow = a[i][j];
                 minRowIndex = i;
                 minColumnIndex = j;
             }
         }
-        // check column: our points are true?
         isNotTrueMinPoint = 0;
         isNotTrueMaxPoint = 0;
         for (ii = 0; ii < m; ii++) {
@@ -54,12 +49,10 @@ void main() {
             }
         }
         if (!isNotTrueMaxPoint) {
-            // save this point as true point
             printf("[%d,%d]\n", maxRowIndex, maxColumnIndex);
             hasPoints = 1;
         }
         if (!isNotTrueMinPoint) {
-            // save this point as true point
             printf("[%d,%d]\n", minRowIndex, minColumnIndex);
             hasPoints = 1;
         }
