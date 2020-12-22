6e3614a9bddaaa5de945032260516d4fe54d848e
ABondarchu <76469391+ABondarchu@users.noreply.github.com>
2020-12-22 16:50:08 +0300

    Update 6.1.c

--- lb-6/6.1.c
+++ lb-6/6.1.c
@@ -45,7 +45,6 @@ int compozitionOfHonestNumber(int *arr, int lenght)
 	for (int i = 1; (2*i-1) < lenght; i++)
 	{
 		compozition = compozition * *(arr + (2*i-1)); 
-		//printf("%d\n", compozition );
 	}
 	return compozition;
 }
@@ -59,7 +58,6 @@ int sumOfNumber(int *arr, int lenght)
     	if (*(arr + i) == 0)
 	 	{
 	 		f_zero = i;
-	 	//	printf("%d\n", i);
 	 		break;
         }
     }
@@ -68,7 +66,6 @@ int sumOfNumber(int *arr, int lenght)
     {
     	if (*(arr + i) == 0)
 	 	{
-	 	//	printf("%d\n", i);
 	 		l_zero = i;
 	 		break;
         }
